fea87908d85552f07323a33494a8c1d27f8dc8e7
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-10-06 17:09:56 +0300

    вап

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,6 @@
 import s from "./SearchBox.module.css";  
 import { useDispatch, useSelector } from "react-redux";  
-import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";  
+import { changeFilter, selectNameFilter } from "./../redux/filtersSlice";  
 
 function SearchBox() {  
   const dispatch = useDispatch();  
