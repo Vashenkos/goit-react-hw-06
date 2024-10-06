3ae212a4ebdc3b2035ae9a279b8db5c7968b6798
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-10-06 17:14:49 +0300

    іі

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { persistor, store } from "./components/redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import "./index.css";
 import "modern-normalize";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
+  </Provider>
 );
