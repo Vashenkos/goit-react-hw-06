d3907ed26870812add965f355693524c0689278f
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-10-06 17:07:59 +0300

    пр

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,32 @@
-import { FaPhone, FaUser } from "react-icons/fa6";
-import { deleteContact } from "../redux/contactsSlice";
-import { useDispatch } from "react-redux";
-import s from "./Contact.module.css"
+import { FaPhone, FaUser } from "react-icons/fa6";  
+import { deleteContact } from "../redux/contactsSlice";  
+import { useDispatch } from "react-redux";  
+import s from "./Contact.module.css";  
 
+function Contact({ contact }) {  
+  const dispatch = useDispatch();  
 
-function Contact({ contact }) {
-  const dispatch = useDispatch();
+  return (  
+    <li className={s.list}>  
+      <div className={s.contdiv}>  
+        <p>  
+          <FaUser className={s.icon} />  
+          {contact.name}  
+        </p>  
+        <p>  
+          <FaPhone className={s.icon} />  
+          {contact.number}  
+        </p>  
+      </div>  
 
-  return (
-    <li className ={s.list}>
-      <div className ={s.contdiv}>
-        <p className>
-          <FaUser className={s.icon}/>    
-              {contact.name}
-        </p>
-        <p className>
-          <FaPhone className={s.icon} />
-             {contact.number}
-        </p>
-      </div>
-
-      <button className={s.buttonlist}
-        onClick={() =>dispatch(deleteContact(contact.id)
-      >
-        Delete
-      </button>
-    </li>
-  );
-}
+      <button  
+        className={s.buttonlist}  
+        onClick={() => dispatch(deleteContact(contact.id))}  
+      >  
+        Delete  
+      </button>  
+    </li>  
+  );  
+}  
 
 export default Contact;
